538b87d112846c43ddf504183f2103bfea6e03d7
Antonio Russo <arusso@sandia.gov>
2022-04-06 21:38:21 -0600

    Implement IPC client as a Backend
    Signed-off-by: Antonio Russo <arusso@sandia.gov>

--- src/jaqalpaq/ipc/ipc.py
+++ src/jaqalpaq/ipc/ipc.py
@@ -5,156 +5,105 @@
 (internal) All members of this module should be considered internal API.
 """
 
-import os
-import socket
-import json
 import math
-import select
+import os
+import socket, select
+import json
 
 from jaqalpaq.generator import generate_jaqal_program
 from jaqalpaq.core.result import ExecutionResult, RelativeFrequencySubcircuit
 from jaqalpaq.error import JaqalError
+from jaqalpaq.run.backend import IndependentSubcircuitsBackend
 
 
-def run_jaqal_string(jaqal):
-    """Run this Jaqal string. This communicates with an existing server."""
-    send_jaqal(jaqal)
-    return receive_response()
+class IPCBackend(IndependentSubcircuitsBackend):
+    def __init__(self, *, address=None, host_socket=None):
+        super().__init__()
+        if host_socket is not None:
+            self._host_socket = host_socket
+            if address is not None:
+                raise TypeError("host_socket and address cannot both be specified")
+        else:
+            if address is None:
+                try:
+                    ipc_port = int(os.environ["JAQALPAQ_RUN_PORT"])
+                except (KeyError, ValueError) as e:
+                    raise TypeError("host_socket or address must be specified")
+                address = ("localhost", ipc_port)
 
+            self._address = address
 
-def run_jaqal_file(fname):
-    with open(fname, "r") as fd:
-        return run_jaqal_string(fd.read())
-
-
-def run_jaqal_circuit(circ):
-    jaqal = generate_jaqal_program(circ)
-    return run_jaqal_string(jaqal)
-
-
-def send_jaqal(jaqal):
-    """Send the jaqal text to the host."""
-    sock = _get_host_socket()
-    sock.send(jaqal.encode())
-
-
-def receive_response():
-    """Wait until we receive a response from the Jaqal we sent."""
-    # The response is serialized JSON. Each entry in the array is a measurement
-    # in the Jaqal file, and each entry in those entries represents
-    sock = _get_host_socket()
-    resp_list = []
-    polling_timeout = 0.1
-    started = False
-    while True:
-        block_size = 4096  # size recommended by Python docs
-        events = select.select([sock], [], [sock], polling_timeout)
-        if any(events):
-            packet = sock.recv(block_size)
-            if packet:
-                resp_list.append(packet.decode())
-                started = True
-                continue
-
-        if started:
-            break
-    resp_text = "".join(resp_list)
-
-    # Deserialize the JSON into a list of lists of floats
-    try:
-        results = json.loads(resp_text)
-    except Exception as exc:
-        print(resp_text)
-        raise JaqalError(f"Bad response: {exc}") from exc
-
-    # Validate the format of the returned JSON
-    results = validate_response(results)
-
-    qubit_count = math.log2(len(results[0]))
-    if 2**qubit_count != len(results[0]):
-        import warnings
-
-        warnings.warn("Invalid frequencies")
-
-    # Create a readout and subcircuit for each inner list. This isn't
-    # quite right but is good enough for what we're doing.
-    subcircuits = []
-    for pidx, rfreqs in enumerate(results):
-        subcircuits.append(IpcSubcircuit(pidx, qubit_count, rfreqs))
-
-    # Combine into an ExecutionResult object
-    er = ExecutionResult(subcircuits)
-    return er
-
-
-def validate_response(results):
-    """Make sure the JSON-decoded results from the host have the expected
-    format."""
-    ret = []
-    for sidx, subcirc in enumerate(results):
-        probs = [float(p) for p in subcirc]
-        if not math.isclose(sum(probs), 1.0):
-            raise JaqalError(f"Line {sidx} of results did not sum to 1")
-        ret.append(probs)
-    return ret
-
-
-##
-# Redefined results classes
-#
-
-
-class IpcSubcircuit(RelativeFrequencySubcircuit):
-    def __init__(self, index, qubit_count, relative_frequencies):
-        super().__init__(None, index, relative_frequencies=relative_frequencies)
-        self._qubit_count = qubit_count
-
-    @property
-    def measured_qubits(self):
-        return [None for i in range(self._qubit_count)]
-
-    def __repr__(self):
-        return f"<{type(self).__name__}>"
-
-
-##
-# Private helper functions
-#
-
-
-def _get_host_socket():
-    """Return a socket to communicate with the host, creating it if
-    necessary. The host is a singleton and cannot be changed during the
-    execution of the program."""
-
-    global _host_socket
-
-    if _host_socket is None:
-        address = ("localhost", _get_port())
+    def get_host_socket(self):
         try:
-            _host_socket = socket.create_connection(address)
+            return self._host_socket
+        except AttributeError:
+            pass
+
+        try:
+            return socket.create_connection(self._address)
         except Exception as exc:
-            raise JaqalError(f"Could not connect to host: {exc}")
+            raise JaqalError(f"Could not connect to host {':'.join(address)}: {exc}")
 
-    return _host_socket
+    def _communicate(self, socket, data):
+        socket.send(data)
 
+        # The response is serialized JSON. Each entry in the array is a measurement
+        # in the Jaqal file, and each entry in those entries represents
+        resp_list = []
+        polling_timeout = 0.1
+        started = False
+        while True:
+            block_size = 4096  # size recommended by Python docs
+            events = select.select([socket], [], [socket], polling_timeout)
+            if any(events):
+                packet = socket.recv(block_size)
+                if packet:
+                    resp_list.append(packet.decode())
+                    started = True
+                    continue
 
-def _get_port():
-    """Return the port to connect to the host or raise an exception if it
-    has not been configured."""
-    port_str = os.environ.get("JAQALPAQ_RUN_PORT")
-    if port_str is None:
-        raise JaqalError("JAQALPAQ_RUN_PORT must be set")
-    try:
-        port = int(port_str)
-    except ValueError:
-        raise JaqalError(
-            f"Could not read a port from the value of JAQALPAQ_RUN_PORT: {port_str}"
-        )
-    return port
+            if started:
+                break
+        resp_text = "".join(resp_list)
 
+        # Deserialize the JSON into a list of lists of floats
+        try:
+            results = json.loads(resp_text)
+        except Exception as exc:
+            print(resp_text)
+            raise JaqalError(f"Bad response: {exc}") from exc
 
-# The socket we've been using to send Jaqal code to our host. By using
-# a global variable we limit the utility of this module. For more
-# advanced uses, the Jaqal Application Framework may be useful.
-_host_socket = None
+        return results
+
+    def _ipc_protocol(self, circuit):
+        jaqal = generate_jaqal_program(circuit)
+        sock = self.get_host_socket()
+        try:
+            results = self._communicate(sock, jaqal.encode())
+        finally:
+            # Can this be reused?
+            sock.close()
+
+        qubit_count = math.log2(len(results[0]))
+        if 2**qubit_count != len(results[0]):
+            import warnings
+
+            warnings.warn("Invalid normalized counts")
+
+        return results
+
+    def _execute_job(self, job):
+        freqs = self._ipc_protocol(job.circuit)
+        n_qubits = self.get_n_qubits(job.circuit)
+        subcircs = []
+        for n, tr in enumerate(job.traces):
+            subcircs.append(
+                RelativeFrequencySubcircuit(
+                    tr, n, relative_frequencies=[float(rf) for rf in freqs[n]]
+                )
+            )
+
+        if n + 1 < len(freqs):
+            raise JaqalError("Unable to parse output: too many values")
+
+        return ExecutionResult(subcircs, None)
--- tests/ipc/test_ipc.py
+++ tests/ipc/test_ipc.py
@@ -1,37 +1,41 @@
 import os, time, sys, subprocess, socket
-from contextlib import contextmanager
 import unittest, pytest
 import numpy
 
-from jaqalpaq.ipc import ipc
+from jaqalpaq.ipc.ipc import IPCBackend
+from jaqalpaq.parser import parse_jaqal_file
+from jaqalpaq.run import run_jaqal_circuit
+
+IPC_SOCKET = "/tmp/ipc_test"
 
 
 @unittest.skipIf(sys.platform.startswith("win"), "IPC tests use Unix sockets")
 class IPCTester(unittest.TestCase):
     """Test interacting through the IPC interface."""
 
-    @contextmanager
-    def mock_server(self):
-        P = subprocess.Popen([sys.executable, "-m", "tests.ipc._mock_server"])
-
-        time.sleep(1)
-        ipc._host_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        ipc._host_socket.connect("/tmp/ipc_test")
+    def run_jaqal(self, circuit, **kwargs):
+        host_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
         try:
-            yield
+            P = subprocess.Popen([sys.executable, "-mtests.ipc._mock_server"])
+            time.sleep(1)
+            host_socket.connect(IPC_SOCKET)
+
+            backend = IPCBackend(host_socket=host_socket)
+            exe = run_jaqal_circuit(circuit, backend=backend, **kwargs)
         finally:
+            host_socket.close()
             P.send_signal(subprocess.signal.SIGTERM)
             P.communicate()
-            ipc._host_socket.close()
-            del ipc._host_socket
+
+        return exe
 
     def test_bell_prep(self):
         """Run bell_prep.jaqal through the IPC mock server, and parse the results"""
 
-        with self.mock_server():
-            exe = ipc.run_jaqal_file("examples/jaqal/bell_prep.jaqal")
+        circ = parse_jaqal_file("examples/jaqal/bell_prep.jaqal")
+        exe = self.run_jaqal(circ)
 
-        sc1, sc2 = exe.subcircuits
-        assert numpy.allclose(sc1.relative_frequency_by_int, [0, 1 / 2, 1 / 2, 0])
-        assert numpy.allclose(sc2.relative_frequency_by_int, [1, 0, 0, 0])
+        sc0, sc1 = exe.subcircuits
+        assert numpy.allclose(sc0.relative_frequency_by_int, [0, 1 / 2, 1 / 2, 0])
+        assert numpy.allclose(sc1.relative_frequency_by_int, [1, 0, 0, 0])
