c7837173462a2331f2fafaab4b1557a4797ea92c
Antonio Russo <arusso@sandia.gov>
2022-06-20 16:32:28 -0600

    Refactor UnitarySerializedEmulator (part 1)
    Signed-off-by: Antonio Russo <arusso@sandia.gov>

--- src/jaqalpaq/emulator/unitary.py
+++ src/jaqalpaq/emulator/unitary.py
@@ -11,6 +11,123 @@ from jaqalpaq.emulator.backend import EmulatedIndependentSubcircuitsBackend
 from ._import import get_ideal_action
 
 
+def matrix_index_iter(gates, gatedefs, ideal_action):
+    first = True
+    must_be_last = False
+    for gate in gates:
+        if must_be_last:
+            raise JaqalError("Invalid gate in subcircuit")
+
+        # This captures the classical arguments to the gate
+        argv = []
+        # This capture the quantum arguments to the gate --- the qubit index
+        qind = []
+        gatedef = gatedefs[gate.name]
+        try:
+            ideal_unitary = ideal_action(gatedef)
+        except KeyError:
+            if first:
+                first = False
+                continue
+            must_be_last = True
+            # maybe add other checks?
+            continue
+        first = False
+
+        if ideal_unitary is None:
+            continue
+
+        for param, val in zip(gatedef.parameters, gate.parameters.values()):
+            if param.classical:
+                argv.append(val)
+            else:
+                qind.append(val.alias_index)
+
+        # This is the dense submatrix
+        dsub = ideal_unitary(*argv)
+        yield dsub, qind
+
+
+def inplace_multiply(matrix_indices, vec):
+    hilb_dim = len(vec)
+
+    # vec = U * inp
+    # We don't need to initialize inp yet
+    inp = numpy.empty(hilb_dim, dtype=complex)
+
+    # The plan is to apply the associated unitary to vec for each gate.
+    for dsub, qind in matrix_indices:
+        # now we need to sparse-multiply:
+        # vec = U * inp
+        # But! U isn't just dsub
+
+        # The current state-vector becomes the input to the matrix multiplication
+        inp, vec = vec, inp
+        # (Notice that this initializes inp, from above)
+        vec[:] = 0
+
+        # For every column in the output matrix, we need to compute the sum
+        # over the nonzero rows of U.
+
+        # However, this corresponds to a sum only over rows of dsub, with
+        # a particular mapping between the rows and columns.
+        for i in range(hilb_dim):
+            # Because we are only dealing with qubits, the binary representation
+            # of the row is precisely the standard basis label corresponding to that
+            # row.
+
+            # We need to re-map the qubits that are being acted on by dsub to
+            # the *column* of dsub.  Additionally, the qubits that are not being
+            # acted on by dsub are unaffected, and therefore are the same in both
+            # the input and output.
+
+            # mask is precisely these bystander qubits --- the affected qubits are
+            # set to zero, and shuffled into another variable, dsub_row
+            mask = i
+
+            # We initialize dsub_row to zero, and then build it up via bit-twiddling
+            dsub_row = 0
+
+            # Which bit (stored as a bitmask) is up to be changed?
+            dsub_bit = 1
+            for i_k in qind:
+                # We iterate over all the qubits acted on by dsub
+
+                # Is this specific qubit high (for this row)?
+                n_high = mask & (1 << i_k)
+
+                # If it is high, lower it in the mask
+                # (notice this is equivalent to subtraction)
+                mask ^= n_high
+
+                # If it is high, raise it in the row to be passed to dsub
+                # (notice this is equivalent to addition)
+                if n_high:
+                    dsub_row |= dsub_bit
+
+                # Advance the bit mask by one
+                dsub_bit <<= 1
+
+            # We now have the row in dsub that corresponds to the row in U
+
+            # Next, we need to iterate over the column of U, and simultaneously the
+            # column of inp --- this mapping is the same as the above, backwards.
+            for dsub_col in range(dsub.shape[0]):
+                j = mask
+                dsub_bit = 1
+                for j_k in qind:
+                    if dsub_col & dsub_bit:
+                        j |= 1 << j_k
+                    dsub_bit <<= 1
+
+                # Suitably armed with the associated row and column, we
+                # do the standard matrix accumulation sum step.
+
+                vec[i] += inp[j] * dsub[dsub_row, dsub_col]
+
+    return vec
+
+
 class UnitarySerializedEmulator(EmulatedIndependentSubcircuitsBackend):
     """Serialized emulator using unitary matrices
 
@@ -35,11 +152,9 @@ class UnitarySerializedEmulator(EmulatedIndependentSubcircuitsBackend):
         n_qubits = self.get_n_qubits(circ)
 
         hilb_dim = 2**n_qubits
-        gatedefs = circ.native_gates
 
         # vec = U * inp
         # We don't need to initialize inp yet
-        inp = numpy.empty(hilb_dim, dtype=complex)
         vec = numpy.zeros(hilb_dim, dtype=complex)
         vec[0] = 1
 
@@ -47,110 +162,10 @@ class UnitarySerializedEmulator(EmulatedIndependentSubcircuitsBackend):
         # The plan is to apply the associated unitary to vec for each gate.
         s = TraceSerializer(trace)
 
-        first = True
-        must_be_last = False
-
-        for gate in s.visit(circ):
-            if must_be_last:
-                raise JaqalError("Invalid gate in subcircuit")
-
-            # This captures the classical arguments to the gate
-            argv = []
-            # This capture the quantum arguments to the gate --- the qubit index
-            qind = []
-            gatedef = gatedefs[gate.name]
-
-            try:
-                ideal_unitary = get_ideal_action(gatedef)
-            except KeyError:
-                if first:
-                    first = False
-                    continue
-                must_be_last = True
-                # maybe add other checks?
-                continue
-            first = False
-
-            if ideal_unitary is None:
-                # This includes things like idle gates
-                continue
-
-            for param, val in zip(gatedef.parameters, gate.parameters.values()):
-                if param.classical:
-                    argv.append(val)
-                else:
-                    qind.append(val.alias_index)
-
-            # This is the dense submatrix
-            dsub = ideal_unitary(*argv)
-
-            # now we need to sparse-multiply:
-            # vec = U * imp
-            # But! U isn't just dsub
-
-            # The current state-vector becomes the input to the matrix multiplication
-            inp, vec = vec, inp
-            # (Notice that this initializes inp, from above)
-            vec[:] = 0
-
-            # For every column in the output matrix, we need to compute the sum
-            # over the nonzero rows of U.
-
-            # However, this corresponds to a sum only over rows of dsub, with
-            # a particular mapping between the rows and columns.
-            for i in range(hilb_dim):
-                # Because we are only dealing with qubits, the binary representation
-                # of the row is precisely the standard basis label corresponding to that
-                # row.
-
-                # We need to re-map the qubits that are being acted on by dsub to
-                # the *column* of dsub.  Additionally, the qubits that are not being
-                # acted on by dsub are unaffected, and therefore are the same in both
-                # the input and output.
-
-                # mask is precisely these bystander qubits --- the affected qubits are
-                # set to zero, and shuffled into another variable, dsub_row
-                mask = i
-
-                # We initialize dsub_row to zero, and then build it up via bit-twiddling
-                dsub_row = 0
-
-                # Which bit (stored as a bitmask) is up to be changed?
-                dsub_bit = 1
-                for i_k in qind:
-                    # We iterate over all the qubits acted on by dsub
-
-                    # Is this specific qubit high (for this row)?
-                    n_high = mask & (1 << i_k)
-
-                    # If it is high, lower it in the mask
-                    # (notice this is equivalent to subtraction)
-                    mask ^= n_high
-
-                    # If it is high, raise it in the row to be passed to dsub
-                    # (notice this is equivalent to addition)
-                    if n_high:
-                        dsub_row |= dsub_bit
-
-                    # Advance the bit mask by one
-                    dsub_bit <<= 1
-
-                # We now have the row in dsub that corresponds to the row in U
-
-                # Next, we need to iterate over the column of U, and simultaneously the
-                # column of inp --- this mapping is the same as the above, backwards.
-                for dsub_col in range(dsub.shape[0]):
-                    j = mask
-                    dsub_bit = 1
-                    for j_k in qind:
-                        if dsub_col & dsub_bit:
-                            j |= 1 << j_k
-                        dsub_bit <<= 1
-
-                    # Suitably armed with the associated row and column, we
-                    # do the standard matrix accumulation sum step.
-
-                    vec[i] += inp[j] * dsub[dsub_row, dsub_col]
+        vec = inplace_multiply(
+            matrix_index_iter(s.visit(circ), circ.native_gates, get_ideal_action),
+            vec,
+        )
 
         probs = numpy.abs(vec) ** 2
 
