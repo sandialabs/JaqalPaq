603d2ac4b5425862a2e20035e5ca91274b81a56b
Antonio Russo <arusso@sandia.gov>
2022-12-19 13:08:14 -0700

    Add a featherlight run_jaqal_batch interface
    Signed-off-by: Antonio Russo <arusso@sandia.gov>

--- src/jaqalpaq/run/__init__.py
+++ src/jaqalpaq/run/__init__.py
@@ -4,6 +4,27 @@
 from .frontend import run_jaqal_circuit, run_jaqal_file, run_jaqal_string
 
 
+class BCResult:
+    """(deprecated) Thin layer providing access to old-style batched Jaqal responses.
+
+    *Only subcircuits* are available through this interface.
+    """
+
+    def __init__(self, circuitindex):
+        self.subcircuits = [circuitindex]
+
+
+def run_jaqal_batch(code, overrides):
+    """(deprecated) Thin later providing access to old-style batched Jaqal.
+
+    BCResult objects are provided, which only allow for access to subcircuit information.
+    """
+    return [
+        BCResult(circuitindex)
+        for circuitindex in run_jaqal_string(code, overrides=overrides).by_time
+    ]
+
+
 __all__ = [
     "run_jaqal_circuit",
     "run_jaqal_file",
