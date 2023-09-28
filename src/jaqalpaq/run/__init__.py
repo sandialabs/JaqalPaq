9d65a28001d5a95a0040fb3eaa815ff91e486631
Antonio Russo <arusso@sandia.gov>
2023-09-28 13:38:29 -0600

    Improve run_jaqal_batch docstring
    Signed-off-by: Antonio Russo <arusso@sandia.gov>

--- src/jaqalpaq/run/__init__.py
+++ src/jaqalpaq/run/__init__.py
@@ -1,6 +1,7 @@
 # Copyright 2020 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
 # Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains
 # certain rights in this software.
+import warnings
 from .frontend import run_jaqal_circuit, run_jaqal_file, run_jaqal_string
 
 
@@ -18,7 +19,18 @@ def run_jaqal_batch(code, overrides):
     """(deprecated) Thin later providing access to old-style batched Jaqal.
 
     BCResult objects are provided, which only allow for access to subcircuit information.
+    Do not use this.  Instead, use run_jaqal_string with an overrides dictionary.
+
+    If using the emulator, you probably also want to set repeats to 1000 (or as desired),
+    either in the overrides dictionary as __repeats__, or by setting
+    jaqalpaq.run.DEFAULT_NUM_REPEATS
+
     """
+    try:
+        import jaqalapp
+    except ImportError:
+        warnings.warn(run_jaqal_batch.__doc__)
+
     return [
         BCResult(circuitindex)
         for circuitindex in run_jaqal_string(code, overrides=overrides).by_time
